bd2e6e392e6f7904cecab7d031aef3967e15cf2c
Deepthinreddy <deepthi.rdy.03@gmail.com>
2026-05-12 12:23:16 +0530

    Import sys and os for path adjustments
    Add system and os imports for path management

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from model.recommender import recommend_outfit
 
 st.set_page_config(
